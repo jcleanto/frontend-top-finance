8ae398644102d673e9aef6cc2d7f3ece0f7827b8
jose.dantas@jazzhr.com <jose.dantas@jazzhr.com>
2025-08-28 16:53:35 -0300

    refactor: add missing ToastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useRoutes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import routes from './router';
 import './App.css'
 
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <>
+      <ToastContainer />
       {content}
     </>
   )
